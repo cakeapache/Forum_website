70e3f7406971d15647d0c555995146ac0d325c21
豈銓 張 <egg2915978@gmail.com>
2019-06-25 06:20:05 +0800

    Layout Demo update 2019/6/25 6:20

--- MyProject/view/FixedBar.php
+++ MyProject/view/FixedBar.php
@@ -1,6 +1,6 @@
 <link rel='stylesheet' type='text/css' href='../css/fixedBar.css?v=1'/>
 <header class='barHeader'>
 <div class='FixedBar'>
-    <a href='index.php'>CakeApache</a>
+    <a href='index.php'><img class='logo' src='../img/Logo.png'></a>
 </div>
 </header>
--- MyProject/view/index.php
+++ MyProject/view/index.php
@@ -5,67 +5,106 @@
 
 
 <div class='container'>
-    <div class='CakeMainBoard'><h3>CakeApache<br></h3>
+    <div class='CakeMainBoard'><h3>Latest News</h3><br>
         <div class='Profile'>
-            <div class='item'><img src='../img/test.jpg' style='max-width:200px; max-height:220px;'><br>
-                test
+            <div class='item'>
+                <a class='p-animate-go'>
+                    <img src='../img/test.jpg' class='item-img'>
+                    <div class='p-wrapper'>
+                        <h2 class='p-animate p-from-left'>
+                            <span>Test1</span>
+                        </h2>
+                    </div>
+                </a>
             </div>
-            <div class='item'><img src='../img/test.jpg' style='max-width:200px; max-height:220px;'><br>
-                test<br>
-                test
+            <div class='item'>
+                <a class='p-animate-go'>
+                    <img src='../img/test.jpg' class='item-img'>
+                    <div class='p-wrapper'>
+                        <h2 class='p-animate p-from-left'>
+                            <span>Test2</span>
+                        </h2>
+                    </div>
+                </a>
             </div>
-            <div class='item'><img src='../img/test.jpg' style='max-width:200px; max-height:220px;'><br>
-                test<br>
-                test
+            <div class='item'>
+                <a class='p-animate-go'>
+                    <img src='../img/test.jpg' class='item-img'>
+                    <div class='p-wrapper'>
+                        <h2 class='p-animate p-from-left'>
+                            <span>Test3</span>
+                        </h2>
+                    </div>
+                </a>
             </div>
-            <div class='item'><img src='../img/test.jpg' style='max-width:200px; max-height:220px;'><br>
-                test<br>
-                test<br>
-                test<br>
-                test
+            <div class='item'>
+                <a class='p-animate-go'>
+                    <img src='../img/test.jpg' class='item-img'>
+                    <div class='p-wrapper'>
+                        <h2 class='p-animate p-from-left'>
+                            <span>Test4</span>
+                        </h2>
+                    </div>
+                </a>
             </div>
-            <div class='item'><img src='../img/test.jpg' style='max-width:200px; max-height:220px;'><br>
-                test<br>
-                test<br>
-                test<br>
-                test
+            <div class='item'>
+                <a class='p-animate-go'>
+                    <img src='../img/test.jpg' class='item-img'>
+                    <div class='p-wrapper'>
+                        <h2 class='p-animate p-from-left'>
+                            <span>Test5</span>
+                        </h2>
+                    </div>
+                </a>
             </div>
-            <div class='item'><img src='../img/test.jpg' style='max-width:200px; max-height:220px;'><br>
-                test<br>
-                test
+            <div class='item'>
+                <a class='p-animate-go'>
+                    <img src='../img/test.jpg' class='item-img'>
+                    <div class='p-wrapper'>
+                        <h2 class='p-animate p-from-left'>
+                            <span>Test6</span>
+                        </h2>
+                    </div>
+                </a>
             </div>
-            <div class='item'><img src='../img/test.jpg' style='max-width:200px; max-height:220px;'><br>
-                test<br>
-                test
+            <div class='item'>
+                <a class='p-animate-go'>
+                    <img src='../img/test.jpg' class='item-img'>
+                    <div class='p-wrapper'>
+                        <h2 class='p-animate p-from-left'>
+                            <span>Test7</span>
+                        </h2>
+                    </div>
+                </a>
             </div>
-            <div class='item'><img src='../img/test.jpg' style='max-width:200px; max-height:220px;'><br>
-                test<br>
-                test<br>
-                test
+            <div class='item'>
+                <a class='p-animate-go'>
+                    <img src='../img/test.jpg' class='item-img'>
+                    <div class='p-wrapper'>
+                        <h2 class='p-animate p-from-left'>
+                            <span>Test8</span>
+                        </h2>
+                    </div>
+                </a>
             </div>
-            <div class='item'><img src='../img/test.jpg' style='max-width:200px; max-height:220px;'><br>
-                test<br>
-                test<br>
-                test<br>
-                test
-            </div>
-            <div class='item'><img src='../img/test.jpg' style='max-width:200px; max-height:220px;'><br>
-                test<br>
-                test
-            </div>
-            <div class='item'><img src='../img/test.jpg' style='max-width:200px; max-height:220px;'><br>
-                test<br>
-                test<br>
-                test
-            </div>
-            <div class='item'><img src='../img/test.jpg' style='max-width:200px; max-height:220px;'><br>
-                test<br>
-                test<br>
-                test
+            <div class='item'>
+                <a class='p-animate-go'>
+                    <img src='../img/test.jpg' class='item-img'>
+                    <div class='p-wrapper'>
+                        <h2 class='p-animate p-from-left'>
+                            <span>Test9</span>
+                        </h2>
+                    </div>
+                </a>
             </div>
         </div>
     </div>
-    <div class='AboutBoard'><h3>About Us<br><br><br><br><br></h3></div>
+    <div class='AboutBoard'>
+        <h3>About Us</h3><br>
+        <h4>We are XXX and XXX</h4>
+        <h5>XXX:123456</h5>
+        <h5>XXX:1234566</h5>
+    </div>
     <div class='ForumBoard'><h3>Forum<br><br><br><br><br></h3></div>
 </div>
 
